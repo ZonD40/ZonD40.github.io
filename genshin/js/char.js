fef422ee1c10e17ec6be255e3c1a2fe90b6f5296
Alexey <vip.leontev2002@gmail.com>
2022-12-18 22:19:37 +0900

    Add genshin site

---
+++ genshin/js/char.js
@@ -0,0 +1,17 @@
+const slider = tns({
+	container: '.my-slider',
+	"items": 1,
+	slideBy: 1,
+	controls: false,
+	navPosition: "bottom",
+	navContainer: ".main__calendar-nav",
+	navAsThumbnails: true,
+	"gutter": 40,
+	"mouseDrag": true,
+	autoplay: false,
+	"fixedWidth": 300,
+	"center": true,
+	loop: false
+  });
+
+
